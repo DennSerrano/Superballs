8ba9f9160fcb0615f555064e48fbbd809200accb
Dennserrano <dennserrano@gmail.com>
2021-10-29 09:56:57 +0200

    superball

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function pressBallOne() {
+    let h1 = document.getElementById("h1");
+    let bg = document.getElementById("bg");
+
+    h1.style.color = "rgb(94, 31, 114)";
+    bg.style.background = "rgb(9, 135, 66)";
+}
+function pressBallTwo() {
+    let h1 = document.getElementById("h1");
+    let bg = document.getElementById("bg");
+
+    h1.style.color = "rgb(247, 228, 15)";
+    bg.style.background = "rgb(10, 74, 113)";
+}
+function pressBallThree() {
+    let h1 = document.getElementById("h1");
+    let bg = document.getElementById("bg");
+
+    h1.style.color = "rgb(240, 99, 12) ";
+    bg.style.background = "rgb(2, 3, 14)";
+}
+function pressBallFour() {
+    let h1 = document.getElementById("h1");
+    let bg = document.getElementById("bg");
+
+    h1.style.color = "rgb(11, 189, 228)";
+    bg.style.background = "rgb(98, 109, 141)";
+}
+
